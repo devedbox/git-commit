b3476c658ab604c67003292857b82e13a7615041
devedbox <862099730@qq.com>
2018-08-07 22:00:58 +0800

    CustomStringConvertible

--- Sources/GitCommitFramework/Errors/GitCommitError.swift
+++ Sources/GitCommitFramework/Errors/GitCommitError.swift
@@ -29,3 +29,28 @@ public enum GitCommitError: Error {
     /// Indicates the bootstrap is duplicate.
     case duplicateBootstrap
 }
+
+// MARK: - CustomStringConvertible
+
+extension GitCommitError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .invalidCommitPath:
+            return "The commit contents' path is invalid."
+        case .emptyCommitContents(atPath: let path):
+            return "The contents of commit at \(path) is empty."
+        case .invalidConfigPath:
+            return "The configuration's path is invalid."
+        case .emptyConfigContents(atPath: let path):
+            return "The contents of configuration at \(path) is empty."
+        case .invalidRange:
+            return "The range is invalid."
+        case .gitRepositoryNotExist(atPath: let path):
+            return "The git repository at \(path) is not existing."
+        case .invalidGitRepository(atPath: let path):
+            return "The git reposotory at \(path) is invalid."
+        case .duplicateBootstrap:
+            return "Duplicate bootstrapping will be ignored."
+        }
+    }
+}
