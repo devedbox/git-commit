9b56ef91534b74cac217c80240368e1b601f537e
devedbox <devedbox@qq.com>
2018-08-07 14:54:16 +0800

    feat: Adds rule to enable ignoring trailing new lines.

--- Sources/GitCommitFramework/Types/GitCommitRule.swift
+++ Sources/GitCommitFramework/Types/GitCommitRule.swift
@@ -22,6 +22,7 @@ public struct GitCommitRule: Decodable {
         case ignoringPattern = "ignoring-pattern"
         case ignoresHashAnchoredLines = "ignores-hash-anchored-lines"
         case allowsReverting = "allows-revert"
+        case ignoresTrailingNewLines = "ignores-trailing-new-lines"
     }
     
     public struct Scope: Decodable {
@@ -35,12 +36,22 @@ public struct GitCommitRule: Decodable {
         public let allowsAsciiPunctuation: Bool?
     }
     
+    /// The types of the commit header. Default would be all values in `GitCommitType.all`.
     public let types: [String]
+    /// The scope of the commit header. Default is:
+    /// - scope:
+    ///   - isRequired: false
     public let scope: Scope
+    /// Indicates the git-commit is enabled or disabled. Default would be `true`.
     public var isEnabled: Bool
+    /// The regex pattern to ignore with. Default is nil.
     public let ignoringPattern: String?
+    /// Should ignore the `#` hash anchor beginning lines. Default is `false`.
     public let ignoresHashAnchoredLines: Bool
+    /// Should allows revert commit. Default is `true`.
     public let allowsReverting: Bool
+    /// Indicates ignores the triling new lines and trimming the trailing new lines when linting.
+    public let ignoresTrailingNewLines: Bool
     
     public init(at path: String) throws {
         guard FileManager.default.fileExists(atPath: path) else {
@@ -64,17 +75,18 @@ public struct GitCommitRule: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        let types = try container.decodeIfPresent([String].self, forKey: CodingKeys.types)
-        let scope = try container.decodeIfPresent(Scope.self, forKey: CodingKeys.scope)
-        let isEnabled = try container.decode(Bool.self, forKey: CodingKeys.isEnabled)
-        let ignoringPattern = try container.decodeIfPresent(String.self, forKey: CodingKeys.ignoringPattern)
-        let ignoresHashAnchoredLines = try container.decodeIfPresent(Bool.self, forKey: CodingKeys.ignoresHashAnchoredLines)
-        let allowsReverting = try container.decodeIfPresent(Bool.self, forKey: CodingKeys.allowsReverting)
+        let types = try container.decodeIfPresent([String].self, forKey: .types)
+        let scope = try container.decodeIfPresent(Scope.self, forKey: .scope)
+        let isEnabled = try container.decode(Bool.self, forKey: .isEnabled)
+        let ignoringPattern = try container.decodeIfPresent(String.self, forKey: .ignoringPattern)
+        let ignoresHashAnchoredLines = try container.decodeIfPresent(Bool.self, forKey: .ignoresHashAnchoredLines)
+        let allowsReverting = try container.decodeIfPresent(Bool.self, forKey: .allowsReverting)
+        let ignoresTrailingNewLines = try container.decodeIfPresent(Bool.self, forKey: .ignoresTrailingNewLines)
         
-        self.init(types: types, scope: scope, isEnabled: isEnabled, ignoringPattern: ignoringPattern, ignoresHashAnchoredLines: ignoresHashAnchoredLines, allowsReverting: allowsReverting)
+        self.init(types: types, scope: scope, isEnabled: isEnabled, ignoringPattern: ignoringPattern, ignoresHashAnchoredLines: ignoresHashAnchoredLines, allowsReverting: allowsReverting, ignoresTrailingNewLines: ignoresTrailingNewLines)
     }
     
-    public init(types: [String]? = nil, scope: Scope? = nil, isEnabled: Bool = true, ignoringPattern: String? = nil, ignoresHashAnchoredLines: Bool? = nil, allowsReverting: Bool? = nil) {
+    public init(types: [String]? = nil, scope: Scope? = nil, isEnabled: Bool = true, ignoringPattern: String? = nil, ignoresHashAnchoredLines: Bool? = nil, allowsReverting: Bool? = nil, ignoresTrailingNewLines: Bool? = nil) {
         
         self.types = types ?? GitCommitType.all.map { $0.rawValue }
         self.scope = scope ?? Scope(isRequired: false, allowsAsciiPunctuation: false)
@@ -82,6 +94,7 @@ public struct GitCommitRule: Decodable {
         self.ignoringPattern = ignoringPattern
         self.ignoresHashAnchoredLines = ignoresHashAnchoredLines ?? false
         self.allowsReverting = allowsReverting ?? true
+        self.ignoresTrailingNewLines = ignoresTrailingNewLines ?? false
     }
 }
 
@@ -115,17 +128,11 @@ extension GitCommitRule: GitCommitRuleRepresentable {
                 .joined(separator: "\n")
         }
         
-        return trimming(charactersIn: .newlines, of: commits)
-        
-        guard ignoresHashAnchoredLines else {
-            return commits
+        if ignoresTrailingNewLines {
+            commits = trimming(charactersIn: .newlines, of: commits)
         }
         
-        // Trimming the hash(`#`) anchored lines if any.
-        return
-            commits.components(separatedBy: CharacterSet.newlines)
-                .filter { !$0.hasPrefix("#") }
-                .joined(separator: "\n")
+        return commits
     }
     
     public func isEnabled(for commits: String) -> Bool {
